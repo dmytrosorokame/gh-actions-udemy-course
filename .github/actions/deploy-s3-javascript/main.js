f47cb7d5a9e4dc6438b2e412c1b460f7ff36233b
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-09-10 07:59:46 +0300

    feat: setup upload to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  // 1) Get inputs
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const sourceDir = core.getInput("source-dir", { required: true });
+
+  // 2) Upload files to S3
+  const s3uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${sourceDir} ${s3uri} --region ${bucketRegion}`);
+
   core.notice("Hello from my custom JavaScript action!");
 }
 
